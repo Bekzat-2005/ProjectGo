b5be3ed1c2b233edfc8c32748fcf23061c04a75c
Bekzat <baymukach@gmail.com>
2025-04-08 22:17:58 +0500

    Commit add limit, page

--- handlers/product_handler.go
+++ handlers/product_handler.go
@@ -15,7 +15,59 @@ var products = []models.Product{
 
 func GetProducts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(products)
+
+	// Читаем query параметры
+	categoryIDStr := r.URL.Query().Get("category_id")
+	limitStr := r.URL.Query().Get("limit")
+	pageStr := r.URL.Query().Get("page")
+
+	var filteredProducts []models.Product
+
+	// Фильтрация по category_id
+	if categoryIDStr != "" {
+		categoryID, err := strconv.Atoi(categoryIDStr)
+		if err == nil {
+			for _, product := range products {
+				if product.CategoryID == categoryID {
+					filteredProducts = append(filteredProducts, product)
+				}
+			}
+		}
+	} else {
+		// Если фильтра нет, берем все продукты
+		filteredProducts = products
+	}
+
+	// Пагинация
+	limit := len(filteredProducts) // по умолчанию все
+	page := 1                      // по умолчанию первая страница
+
+	if limitStr != "" {
+		if l, err := strconv.Atoi(limitStr); err == nil {
+			limit = l
+		}
+	}
+
+	if pageStr != "" {
+		if p, err := strconv.Atoi(pageStr); err == nil {
+			page = p
+		}
+	}
+
+	start := (page - 1) * limit
+	end := start + limit
+
+	// Проверяем границы массива
+	if start > len(filteredProducts) {
+		start = len(filteredProducts)
+	}
+	if end > len(filteredProducts) {
+		end = len(filteredProducts)
+	}
+
+	paginatedProducts := filteredProducts[start:end]
+
+	json.NewEncoder(w).Encode(paginatedProducts)
 }
 
 func GetProduct(w http.ResponseWriter, r *http.Request) {
